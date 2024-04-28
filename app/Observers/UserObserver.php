e94a8effd69d1e9a93a1551f34d0621b0b9375d0
Jefferson Frade <jefferrson.frade@gmail.com>
2024-04-28 05:59:52 -0300

    Adicionado para remover as transacoes

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -3,11 +3,14 @@
 namespace App\Observers;
 use App\Repositories\AccountRepository;
 use App\Repositories\Models\User;
+use App\Repositories\TransferenceRepository;
 
 class UserObserver
 {
     public function deleting(User $user)
     {
         app(AccountRepository::class)->customDelete('id_user', $user->id);
+        app(TransferenceRepository::class)->customDelete('id_payer', $user->id);
+        app(TransferenceRepository::class)->customDelete('id_payee', $user->id);
     }
 }
